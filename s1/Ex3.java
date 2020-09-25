ce941ada0e148efbc526ad33c31a1f7ed2e7a449
CSenshi <saba.pochkhua@gmail.com>
2020-09-25 15:18:02 +0400

    add s1

--- s1/Ex3.java
+++ s1/Ex3.java
@@ -1,12 +1,4 @@
 package s1;
-/*
- * File: CheckerboardKarel.java
- * ----------------------------
- * When you finish writing it, the CheckerboardKarel class should draw
- * a checkerboard using beepers, as described in Assignment 1.  You
- * should make sure that your program works for all of the sample
- * worlds supplied in the starter folder.
- */
 
 import stanford.karel.*;
 
