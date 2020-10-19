f675ee5bc49d3782d4137f305eeb11f4990e7272
CSenshi <saba.pochkhua@gmail.com>
2020-10-19 22:55:24 +0400

    add s9

--- s9/Ex24.java
+++ s9/Ex24.java
@@ -17,16 +17,21 @@ public class Ex24 extends ConsoleProgram {
 	}
 
 	private void printDivisors(int n) {
+		int count = 0;
 		for (int i = 1; i <= Math.sqrt(n); i++) {
 			if (n % i == 0) {
 				int divisor = i;
 				int quotient = n / i;
 
-				println(divisor);
-				if (quotient != divisor)
-					println(quotient);
+				count++;
+				println("Divisor " + count + ": " + divisor);
+				if (quotient != divisor) {
+					count++;
+					println("Divisor " + count + ": " + quotient);
+				}
 			}
 		}
+		println("Total Divisors: " + count);
 	}
 
 }
